6568ce7c9cc8386108f2de33ec545c82197f9836
Anna Spysz <anna.spysz@stackery.io>
2018-11-02 16:23:22 -0700

    added GitHub token validation

--- src/handleWebhook/index.js
+++ src/handleWebhook/index.js
@@ -1,11 +1,21 @@
-module.exports.githubWebhookHandler = (event, context, callback) => {
+const crypto = require('crypto');
+// This function will validate your payload from GitHub
+// See docs at https://developer.github.com/webhooks/securing/#validating-payloads-from-github
+function signRequestBody(key, body) {
+  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
+}
+// The webhook handler function
+exports.githubWebhookHandler = async event => {
   // get the GitHub secret from the environment variables
   const token = process.env.GITHUB_WEBHOOK_SECRET;
-  // determine the type of GitHub event from the header
+  const calculatedSig = signRequestBody(token, event.body);
+  let errMsg;
+  // get the remaining variables from the GitHub event
   const headers = event.headers;
+  const sig = headers['X-Hub-Signature'];
   const githubEvent = headers['X-GitHub-Event'];
   const body = JSON.parse(event.body);
-  // this determines the pusher's username for a push event, but lists the repo owner for other events
+  // this determines username for a push event, but lists the repo owner for other events
   const username = body.pusher ? body.pusher.name : body.repository.owner.login;
   const message = body.pusher ? `${username} pushed this awesomeness/atrocity through (delete as necessary)` : `The repo owner is ${username}.`
   // get repo variables
@@ -15,12 +25,21 @@ module.exports.githubWebhookHandler = (event, context, callback) => {
   
   // check that a GitHub webhook secret variable exists, if not, return an error
   if (typeof token !== 'string') {
-    let errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
-    return callback(null, {
+    errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
+    return {
       statusCode: 401,
       headers: { 'Content-Type': 'text/plain' },
       body: errMsg,
-    });
+    };
+  }
+  // check validity of GitHub token
+  if (sig !== calculatedSig) {
+    errMsg = 'X-Hub-Signature incorrect. Github webhook token doesn\'t match';
+    return {
+      statusCode: 401,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errMsg,
+    };
   }
 
   // print some messages to the CloudWatch console
@@ -31,7 +50,7 @@ module.exports.githubWebhookHandler = (event, context, callback) => {
   console.log('---------------------------------');
 
   // more advanced logic can go here
-
+  
   // return a 200 response if the GitHub tokens match
   const response = {
     statusCode: 200,
@@ -40,5 +59,5 @@ module.exports.githubWebhookHandler = (event, context, callback) => {
     }),
   };
 
-  return callback(null, response);
+  return response;
 };
