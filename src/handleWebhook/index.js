1d3eea0ac65a772e93982e615a82ae219657003b
bildungsroman <anna.spysz@stackery.io>
2020-02-12 16:35:17 -0800

    Update function and config

--- src/handleWebhook/index.js
+++ src/handleWebhook/index.js
@@ -1,11 +1,11 @@
 const crypto = require('crypto');
 // This function will validate your payload from GitHub
 // See docs at https://developer.github.com/webhooks/securing/#validating-payloads-from-github
-function signRequestBody(key, body) {
+function signRequestBody (key, body) {
   return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
 }
 // The webhook handler function
-exports.githubWebhookHandler = async event => {
+exports.handler = async event => {
   // get the GitHub secret from the environment variables
   const token = process.env.GITHUB_WEBHOOK_SECRET;
   const calculatedSig = signRequestBody(token, event.body);
@@ -17,19 +17,19 @@ exports.githubWebhookHandler = async event => {
   const body = JSON.parse(event.body);
   // this determines username for a push event, but lists the repo owner for other events
   const username = body.pusher ? body.pusher.name : body.repository.owner.login;
-  const message = body.pusher ? `${username} pushed this awesomeness/atrocity through (delete as necessary)` : `The repo owner is ${username}.`
+  const message = body.pusher ? `${username} pushed this awesomeness/atrocity through (delete as necessary)` : `The repo owner is ${username}.`;
   // get repo variables
   const { repository } = body;
   const repo = repository.full_name;
   const url = repository.url;
-  
+
   // check that a GitHub webhook secret variable exists, if not, return an error
   if (typeof token !== 'string') {
     errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
     return {
       statusCode: 401,
       headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
+      body: errMsg
     };
   }
   // check validity of GitHub token
@@ -38,7 +38,7 @@ exports.githubWebhookHandler = async event => {
     return {
       statusCode: 401,
       headers: { 'Content-Type': 'text/plain' },
-      body: errMsg,
+      body: errMsg
     };
   }
 
@@ -50,13 +50,13 @@ exports.githubWebhookHandler = async event => {
   console.log('---------------------------------');
 
   // more advanced logic can go here
-  
+
   // return a 200 response if the GitHub tokens match
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      input: event,
-    }),
+      input: event
+    })
   };
 
   return response;
